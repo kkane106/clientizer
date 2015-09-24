df4b65368ab6701650f4989c3cbc9a0ead47406f
kkane106 <kkane106@gmail.com>
2015-09-24 07:13:40 -0600

    Add Secure User Account Creation
    Add bcrypt gem dependency
    Add user controller (and routes)
    Add user model (including has_secure_password)
    Add user new view/partial
    
    Add SessionsController (and routes)
    Add login view/partial
    
    Add User migration

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150923231409) do
+ActiveRecord::Schema.define(version: 20150924125841) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -28,6 +28,14 @@ ActiveRecord::Schema.define(version: 20150923231409) do
     t.text     "notes"
     t.string   "city"
     t.string   "state"
+  end
+
+  create_table "users", force: true do |t|
+    t.string   "name"
+    t.string   "email"
+    t.string   "password_digest"
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
 end
